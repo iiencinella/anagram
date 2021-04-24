8c376c0bc0e3478f76072ea7d655034f4267d4df
Iván Ezequiel Iencinella <iiencinella@outlook.es>
2021-04-24 00:36:05 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+
+namespace anagram
+{
+   class Program
+   {
+      
+      static void Main(string[] args)
+      {
+         string wordOne;
+         string wordTwo;
+
+         Console.WriteLine("Bienvenido!!!");
+         Console.WriteLine("Esto es un ejercicio para determinar si una palabra es anagrama de otra.");
+         
+         Console.WriteLine("Ingrese la primer palabra (es para evaluar si es palabra anagram)");
+         wordOne = Console.ReadLine();
+         
+         Console.WriteLine("Ingrese la segunda palabra");
+         wordTwo = Console.ReadLine();
+
+         //First validation. Lenghts words
+         if(wordOne.Length != wordTwo.Length)
+         {
+            Console.WriteLine("Las palabras no son anagrama");
+            Thread.Sleep(1000);
+            return;
+         }
+
+         if(AnagramRemove(ref wordOne, ref wordTwo))
+         {
+            Console.WriteLine("Las palabras son anagramas");
+         }
+         else
+         {
+            Console.WriteLine("Las palabras no son anagrama");
+         }
+         Thread.Sleep(1000);
+      }
+
+      static private bool AnagramRemove(ref string word1, ref string word2)
+      {
+         for (int i = 0; i < word1.Length; i++)
+         {
+            int pos = word2.IndexOf(word1[i]);
+            if (pos > -1) word2 = word2.Remove(pos, 1);
+            else break;
+         }
+
+         return word2 == "";
+      }
+   }
+}
---
+++ README.txt
@@ -0,0 +1,3 @@
+﻿Este programa es para implementar distintas soluciones al ejercicio de Anagrama.
+
+1- Realizar una verificicación de existencia de caracteres de la primer palabra en la segunda.
